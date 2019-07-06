2f6fd4e60d693d5b4e9227a79308942eafa34e3c
Elshad <safarov.elshad007@gmail.com>
2019-07-06 19:03:31 +0500

    Added SwapiService class in index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-function App(){
-    const getResource = async (url) => {
-        const response = await fetch(url);
+class SwapiService{
+    constructor(){
+        this._apiBase = 'https://swapi.co/api';
+    }
+
+    async getResource(url) {
+        const response = await fetch(`${this._apiBase}${url}`);
         if(!response.ok){
             throw new Error(`Could not found ${url} received ${response.status}`);
         }
-        const body = await response.json();
-        return body;
+        return await response.json();
     }
-    getResource('https://swapi.co/api/people/1/')
-        .then((body) => {
-            console.log(body);
-        })
-        .catch((error) => {
-            console.error('Could not fetch', error);
+
+    async getAllPeople(){
+        const res = await this.getResource('/people/');
+        return res.results;
+    }
+
+    getPerson(id){
+        return this.getResource(`/people/${id}/`);
+    }
+    
+    async getAllPlanet(){
+        const res = await this.getResource('/planets/');
+        return res.results;
+    }
+
+    getPlanet(id){
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarships(){
+        const res = await this.getResource(`/starships/`); 
+        return res.results;
+    }
+
+    getStarship(id){
+        return this.getResource(`/starships/${id}`);
+    }
+}
+
+function App(){
+    const swapi = new SwapiService();
+    swapi.getAllStarships()
+        .then((starships) => {
+            starships.forEach((starship) => {
+                console.log(starship.name);
+            });
+        });
+    swapi.getStarship(3)
+        .then((starship) => {
+            console.log(starship);
         });
     return(
         <h1>Hello World</h1>
