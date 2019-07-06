ce465013e6a96060954fd69bdb2a8da780ab6816
Elshad <safarov.elshad007@gmail.com>
2019-07-06 17:37:47 +0500

    Receive data from server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 function App(){
+    const getResource = async (url) => {
+        const response = await fetch(url);
+        const body = await response.json();
+        return body;
+    }
+    getResource('https://swapi.co/api/people/1/')
+        .then((body) => {
+            console.log(body);
+        });
     return(
         <h1>Hello World</h1>
     );
