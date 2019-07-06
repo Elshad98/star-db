b3c25482b95ec21eace4283938334b5bc3418d80
Elshad <safarov.elshad007@gmail.com>
2019-07-06 17:46:42 +0500

    Added error handling in fatch

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import * as serviceWorker from './serviceWorker';
 function App(){
     const getResource = async (url) => {
         const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`Could not found ${url} received ${response.status}`);
+        }
         const body = await response.json();
         return body;
     }
     getResource('https://swapi.co/api/people/1/')
         .then((body) => {
             console.log(body);
+        })
+        .catch((error) => {
+            console.error('Could not fetch', error);
         });
     return(
         <h1>Hello World</h1>
